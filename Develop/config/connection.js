59e9a6f346135f3f444e257e35e07514aa144691
eli-soto <142184979+eli-soto@users.noreply.github.com>
2023-11-30 23:43:49 -0500

    models

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
+require('dotenv').config(); // DOTENV is to keep secret 
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.JAWSDB_URL //if statement 
+  ? new Sequelize(process.env.JAWSDB_URL) //?then run the code 
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    // : means else
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+// if s
+
 
 module.exports = sequelize;
